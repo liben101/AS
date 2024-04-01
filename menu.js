52d5dbc03784f0650631eb2ce22d8749109a4493
ben.li <53158397+liben101@users.noreply.github.com>
2024-04-01 14:50:34 +0800

    1

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 export const menuList = [
     {
         label: '请检流程图示例',
-        path: '/思维导图/龙鑫MES/请检流程图示例/检测流程举例.drawio.html'
+        path: './思维导图/龙鑫MES/请检流程图示例/检测流程举例.drawio.html'
     }
 ]
