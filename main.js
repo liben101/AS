764bb80628e9209275856dcff7c0e3ef2c0a04d3
ben.li <53158397+liben101@users.noreply.github.com>
2024-04-01 15:57:51 +0800

    1

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-import {menuList} from './menu.js'
+import { menuList } from './menu.js'
 
-let _html = ``
 
-menuList.forEach(_item=>{
-    _html+= `<a target="_blank" href="${_item.path}">${_item.label}</a>`
-})
+// menuList.forEach(_item => {
+//     _html += `<a target="_blank" href="${_item.path}">${_item.label}</a><br/>`
+// })
 
-document.querySelector("#app").innerHTML = _html
+function generateMenuHtml(children, hierarchy) {
+    let _html = ''
+    children.forEach(element => {
+        if (!element.folderFlag) {
+            _html += `<a style="margin-left: ${hierarchy * 30}px" target="_blank" href="${element.path}">${element.label}</a><br/>`
+        } else {
+            _html += `<span style="margin-left: ${hierarchy * 30}px; border-bottom: 2px solid black">${element.label}</span><br/>`
+        }
+
+        if (element.children) {
+            _html += generateMenuHtml(element.children, hierarchy + 1)
+        }
+    });
+
+    return _html
+
+}
+
+document.querySelector("#app").innerHTML = generateMenuHtml(menuList, 0)
+
+
--- menu.js
+++ menu.js
@@ -1,6 +1,17 @@
 export const menuList = [
     {
-        label: '龙鑫MES请检流程图示例_EXAMPLE',
-        path: './思维导图/龙鑫MES/请检流程图示例/检测流程举例.drawio.html'
+        label: '龙鑫MES相关整理',
+        folderFlag: true,
+        children: [
+            {
+                label: '龙鑫MES请检流程图示例_EXAMPLE',
+                path: './assets/龙鑫MES/请检流程图示例/检测流程举例.drawio.html'
+            },
+            {
+                label: '龙鑫MES质量/检化验模块设计期望',
+                path: './assets/龙鑫MES/质量模块/检化验模块设计期望/质量检化验管理.pdf'
+            },
+        ]
     }
+
 ]
