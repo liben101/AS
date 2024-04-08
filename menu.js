ac34f8ce52081d14847b03244fe49b8e78b18402
ben.li <53158397+liben101@users.noreply.github.com>
2024-04-08 15:13:52 +0800

    1

--- menu.js
+++ menu.js
@@ -39,13 +39,30 @@ export const menuList = [
                 // ]
             },
             {
-                label: '设计器菜单',
-                path: './assets/supOS相关/设计期菜单/设计器菜单.pdf'
+                label: 'supOS操作流程',
+                path: './assets/supOS相关/supOS操作流程 copy.pdf',
+                // children: [
+                //     {
+                //         label: '龙鑫MES请检流程图示例_EXAMPLE',
+                //         path: './assets/龙鑫MES/请检流程图示例/检测流程举例.drawio.html',
+                //     }
+                // ]
             },
             {
-                label: '接入管理',
-                path: './assets/supOS相关/接入管理/接入管理菜单.pdf'
-            }
+                label: '运行器菜单',
+                folderFlag: true,
+            },
+            {
+                label: '设计器菜单',
+                path: './assets/supOS相关/设计期菜单/设计器菜单.pdf',
+                children: [
+                    {
+                        label: '接入管理',
+                        path: './assets/supOS相关/设计期菜单/接入管理/接入管理菜单.pdf'
+                    }
+                ]
+            },
+        
            
 
         ]
